99bdefa4decff152bf6d936d1d175bb49aa08c75
kangdari_ <ksh44820@naver.com>
2020-01-22 16:52:01 +0900

    로그인 구현

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,14 +1,18 @@
 // 리덕스와 연동을 위한 컨테이너 컴포넌트
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }));
 
     // 인풋 변경 인벤트 핸들러
@@ -26,15 +30,35 @@ const LoginForm = () => {
 
     // 폼 등록 이벤트 핸들러
     const onSubmit = e => {
-        console.log('click')
         e.preventDefault();
-        //
+        const { username, password } = form;
+        dispatch(login({ username, password }));
     };
 
     // 컴포넌트가 처음 렌더링될 때 form을 초기화
     useEffect(() => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
+
+    // 로그인 성공 / 실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('로그인 성공');
+            console.log(auth);
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if (user) {
+            history.push('/');
+        }
+    }, [history, user]);
 
     // 렌더링
     return (
@@ -47,4 +71,4 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
